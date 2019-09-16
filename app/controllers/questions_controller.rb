f49a778055cbd121ebbe7ddcf8b61726fd411306
Agnieszka Stec <agnieszkaa.stec@gmail.com>
2019-09-16 14:33:30 +0200

    challenge 2 working on coach answer page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,14 @@
 class QuestionsController < ApplicationController
-
-
   def ask
-  end
+end
 
   def answer
+    if params[:question] == 'I am going to work'
+      @answer == 'Great!'
+    elsif params[:question].include? '?'
+      @answer == 'Stupid question! Get dressed and go back to world!'
+    else
+      @answer == 'I don\'t care! Get dressed and go back to work!'
+    end
   end
 end
