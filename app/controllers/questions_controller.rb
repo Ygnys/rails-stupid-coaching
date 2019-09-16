db2d0398e4b8c2ed2c26161e33328e4b0eae00ad
Agnieszka Stec <agnieszkaa.stec@gmail.com>
2019-09-16 15:24:41 +0200

    ask answer started to work

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,14 +1,13 @@
 class QuestionsController < ApplicationController
-  def ask
-end
-
+    def ask
+    end
   def answer
     if params[:question] == 'I am going to work'
-      @answer == 'Great!'
+      @answer = 'Great!'
     elsif params[:question].include? '?'
-      @answer == 'Stupid question! Get dressed and go back to world!'
+      @answer = 'Stupid question! Get dressed and go back to world!'
     else
-      @answer == 'I don\'t care! Get dressed and go back to work!'
+      @answer = 'I don\'t care! Get dressed and go back to work!'
     end
   end
 end
